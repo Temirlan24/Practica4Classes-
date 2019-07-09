30c27304add2eddbf3dca06de9d8052067883afd
Temirlan24 <52044447+Temirlan24@users.noreply.github.com>
2019-07-09 09:49:44 +0600

    Практика №4. Создал класс Money.

---
+++ ConsoleApp1/Classes/Money.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public partial class Money
+    {
+        //Поля
+        private int valueMoney; // номинал купюры
+        private int quantityMoney; //колличество купюр 
+                                   // Конструктор 
+
+        public Money(int banknotes, int number)
+        {
+            this.valueMoney = banknotes;
+            this.quantityMoney = number;
+        }
+        //Методы
+        public int getValueMoney()
+        {
+            return valueMoney;
+        }
+        public void setValueMoney(int valueMoney)
+        {
+            this.valueMoney = valueMoney;
+        }
+
+        public int getQuantityMoney()
+        {
+            return quantityMoney;
+        }
+        public void setQuantityMoney(int quantityMoney)
+        {
+            this.quantityMoney = quantityMoney;
+        }
+
+        public void checkMoney(int totalCost)
+        {
+            int totalValue;
+            totalValue = valueMoney * quantityMoney;
+            if (totalValue >= totalCost)
+            {
+                Console.WriteLine("Денежные средства хватают на покупку");
+            }
+            else
+            {
+                Console.WriteLine("Денежных средств не хватает на покупку");
+            }
+        }
+        public void defeineQuantity(int productCost)
+        {
+            int totalValue;
+            totalValue = valueMoney * quantityMoney;
+            int quantityOfProduct = totalValue / productCost;
+            Console.WriteLine("За {0}тг можно купить {1} товара", productCost, quantityOfProduct);
+
+        }
+
+        public int totalMoney()
+        {
+            return valueMoney * quantityMoney;
+        }
+    }
+}
---
+++ ConsoleApp1/Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money dengi = new Money(1000, 10);
+            Console.WriteLine("Общая сумма = " + dengi.totalMoney());
+            dengi.checkMoney(20000);
+            dengi.defeineQuantity(200);
+
+            //Console.WriteLine(dengi.totalMoney()) ;
+            Console.ReadKey();
+        }
+    }
+}
